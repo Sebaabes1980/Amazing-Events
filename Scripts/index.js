e25e516b24f69f2f68c8061bcbd9efec3beba117
Sebaabes80 <96926772+Sebaabes80@users.noreply.github.com>
2023-03-10 17:58:23 -0300

    10-03

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,74 +1,28 @@
-import data from "./data.js";
+import { imprimirCards, createCategories, createChecks, filterSearch, filterChecks } from './helpers.js';
+
 let $container = document.getElementById("contenedor-cards");
 let $checks = document.getElementById("contenedor-check");
 const $search = document.querySelector('input[placeholder="Search"]');
-const fragment = document.createDocumentFragment();
-function imprimirCards(array, contenedor){
-    contenedor.innerHTML=""
-    for(let event of array){
-        let div = document.createElement('div');
-        div.className = "card"
-        div.innerHTML += `
-        <h5 class="card-title">${event.category}</h5>
-        <img class="card-img-top"  src="${event.image}"/>
-        <div class="card-body">          
-          <p class="card-text">${event.description}</p>          
-        </div>
-        <a href="details.html?id=${event._id}" class="btn btn-primary">${event.name}</a>
-                ` 
-        fragment.appendChild(div)        
-    }     
-    contenedor.appendChild(fragment)
-}
+const $reset = document.getElementById('reset');
+const $spinner = document.getElementById('spinner');
+let data = [];
+let categories = "";
+
+const showSpinner = () => {
+    $spinner.classList.add('spinner--active');
+};
+
+const hideSpinner = () => {
+    $spinner.classList.remove('spinner--active');
+};
+
+createCategories(data.events)
 
 imprimirCards(data.events, $container)
 
-const createCategories = (array) =>{
-  let categories = array.map(cat=> cat.category)
-
-  categories = categories.reduce((acumulador, elemento)=>{
-      if(!acumulador.includes(elemento)){
-          acumulador.push(elemento);
-      }
-      return acumulador
-  }, [])
-  return categories 
-}
-
-let categories = createCategories(data.events)
-
-const createChecks = (array, container) => {
-    array.forEach( category=>{
-        let div = document.createElement('div')
-        div.className = `form-check ${category.toLowerCase()}`
-        div.innerHTML = `
-        <input class="form-check-input" type="checkbox" value="${category.toLowerCase()}" id="${category.toLowerCase()}">
-        <label class="form-check-label" for="${category.toLowerCase()}">
-          ${category}
-        </label>
-        `
-        container.appendChild(div)
-    })
-}
 
 createChecks(categories, $checks)
 
-const filterSearch = (array, value) => {
-    let filteredArray = array.filter(element=> element.name.toLowerCase().includes(value.toLowerCase().trim()))
-    return filteredArray
-}
-
-const filterChecks = (array) => {
-    let checked = document.querySelectorAll(`input[type="checkbox"]:checked`);
-    let checkMapeado = Array.from(checked).map(elemento=> elemento.value)
-    console.log(checkMapeado)
-    let filteredArray = array.filter(element => checkMapeado.includes(element.category.toLowerCase()))
-    console.log(filteredArray.length)
-    if (filteredArray.length < 1) {
-        return array
-    }
-    return filteredArray
-}
 
 const filterAndPrint =  (array) =>{
     let arrayFiltered = filterChecks(array)
---
+++ scripts/helpers.js
@@ -0,0 +1,61 @@
+const fragment = document.createDocumentFragment();
+
+export function imprimirCards(array, contenedor){
+    contenedor.innerHTML=""
+    for(let event of array){
+        let div = document.createElement('div');
+        div.className = "card"
+        div.innerHTML += `
+        <h5 class="card-title">${event.category}</h5>
+        <img class="card-img-top"  src="${event.image}"/>
+        <div class="card-body">          
+          <p class="card-text">${event.description}</p>          
+        </div>
+        <a href="details.html?id=${event._id}" class="btn btn-primary">${event.name}</a>
+                ` 
+        fragment.appendChild(div)        
+    }     
+    contenedor.appendChild(fragment)
+}
+
+export const createCategories = (array) =>{
+    let categories = array.map(cat=> cat.category)  
+    categories = categories.reduce((acumulador, elemento)=>{
+        if(!acumulador.includes(elemento)){
+            acumulador.push(elemento);
+        }
+        return acumulador
+    }, [])
+    return categories 
+}
+
+export const createChecks = (array, container) => {
+    array.forEach( category=>{
+        let div = document.createElement('div')
+        div.className = `form-check ${category.toLowerCase()}`
+        div.innerHTML = `
+        <input class="form-check-input" type="checkbox" value="${category.toLowerCase()}" id="${category.toLowerCase()}">
+        <label class="form-check-label" for="${category.toLowerCase()}">
+          ${category}
+        </label>
+        `
+        container.appendChild(div)
+    })
+}
+
+export const filterSearch = (array, value) => {
+    let filteredArray = array.filter(element=> element.name.toLowerCase().includes(value.toLowerCase().trim()))
+    return filteredArray
+}
+
+export const filterChecks = (array) => {
+    let checked = document.querySelectorAll(`input[type="checkbox"]:checked`);
+    let checkMapeado = Array.from(checked).map(elemento=> elemento.value)
+    console.log(checkMapeado)
+    let filteredArray = array.filter(element => checkMapeado.includes(element.category.toLowerCase()))
+    console.log(filteredArray.length)
+    if (filteredArray.length < 1) {
+        return array
+    }
+    return filteredArray
+}
